810fc9c7f474931c02f568f64c876297a5589e25
Billy <ryotabilly@gmail.com>
2021-05-26 13:30:26 -0700

    Implement the base of video generator

--- VideoGenerator.py
+++ VideoGenerator.py
@@ -1,58 +1,80 @@
 import cv2
 import numpy as np
 import time
+
+
 class VideoGenerator():
     cam = None
+    frame = None
     operation = 'n'
+    haltOp = False
+    fpsOp = False
+    colorOp = False
+
     def __init__(self):
-        #Chose camera option
+        # Chose camera option
         self.cam = cv2.VideoCapture(1)
-        self.cam.set(cv2.CAP_PROP_FRAME_WIDTH,1920)
-        self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT,1080)
+        self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+        
 
     def run(self):
         while True:
-            #Read each frame from VideoCapture
-            ret, frame = self.cam.read()
-            frame = self.frameRunner(frame)
-            cv2.imshow('window',frame)
-
-            #display a frame for 1 ms, after which display will be automatically closed.
+            # Read each self.frame from VideoCapture
+            ret, self.frame = self.cam.read()
+            self.frameRunner()
+            if(not self.haltOp):
+                cv2.imshow('window', self.frame)
             inp = cv2.waitKey(1)
-            if(not inp==-1):
+            
+            if(not inp == -1):
                 self.operation = inp
+            else:
+                self.operation = ''
+
         # When everything done, release the capture
         self.cam.release()
         cv2.destroyAllWindows()
-        
-    def frameRunner(self,frame):
-        if(self.operation==ord('n')):
-            return frame
-        elif(self.operation==ord('t')):
-            return self.addText(frame)
-        elif(self.operation==ord('f')):
-            return self.changeFrameRate(frame,0.5)
-        elif(self.operation==ord('h')):
-            return self.haltFrameRate(frame)
-        else:
-            return frame
-            
-    def addText(self,frame):
-        cv2.putText(frame,'Hello!',(500,800),cv2.FONT_HERSHEY_SIMPLEX,10,(255, 255, 255))
-        return frame
-    
-    def changeFrameRate(self,frame,fr):
-        #Since self.cam.set(cv2.CAP_PROP_FPS) is not working, using time.sleep
-        time.sleep(fr)
-        return frame
-    
-    def haltFrameRate(self,frame):
-        while(True):
-            inp = cv2.waitKey(1)
-            if(inp==ord('h')):
-                self.operation = 'n'
-                break
+
+    def frameRunner(self):
+        self.frameController()
+        if(self.colorOp):
+            self.frame = self.changeColor(self.frame)
+        if(self.fpsOp):
+            self.frame = self.changeFrameRate(self.frame, 0.5)
+        if(self.haltOp):
+            self.frame = self.haltFrameRate(self.frame)
+        return self.frame
+
+    def frameController(self):
+        if(self.operation == ord('n')):
+            self.colorOp = False
+            self.fpsOp = False
+            self.haltOp = False
+        elif(self.operation == ord('c')):
+            self.colorOp = not self.colorOp
+        elif(self.operation == ord('f')):
+            self.fpsOp = not self.fpsOp
+        elif(self.operation == ord('h')):
+            self.haltOp = not self.haltOp
+
+    def changeColor(self, frame):
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        lower_blue = np.array([110, 50, 50])
+        upper_blue = np.array([130, 255, 255])
+        frame = cv2.inRange(hsv, lower_blue, upper_blue)
         return frame
 
-if  __name__=='__main__':
+    def changeFrameRate(self, frame, fr):
+        # Since self.cam.set(cv2.CAP_PROP_FPS) is not working, using time.sleep
+        time.sleep(fr)
+        return frame
+
+    def haltFrameRate(self, frame):
+        return frame
+    
+    def getFrame(self):
+        return self.self.frame
+
+if __name__ == '__main__':
     VideoGenerator().run()
